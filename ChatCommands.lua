a65e904089f80d2ebfbf76f0084b50b897abda02
XalaMental <s79j2jb2c2@privaterelay.appleid.com>
2026-08-01 21:04:34 -0400

    ChatCommands.lua: Small cleanup - removed a leftover reference to the deleted trail setting. No visible change.

--- ChatCommands.lua
+++ ChatCommands.lua
@@ -14,7 +14,7 @@ local EXCLUDED_KEYS = {
     showPins = true, compassPosition = true, autoAdvanceDistance = true,
     pinStyle = true, pinSize = true, showHelperButton = true,
     helperButtonPosition = true, showGlow = true, proximityDistanceYards = true,
-    showRouteTrail = true, arrowProgressColor = true, pinAlpha = true,
+    arrowProgressColor = true, pinAlpha = true,
     duplicateDistanceYards = true, compassArrowStyle = true,
 }
 
