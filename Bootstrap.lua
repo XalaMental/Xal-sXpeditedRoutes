b40f9c8021493f025a329f781473c4bb8886719d
XalaMental <s79j2jb2c2@privaterelay.appleid.com>
2026-08-02 09:47:44 -0400

    Minor cleanup, removed an old outdated comment.

--- Bootstrap.lua
+++ Bootstrap.lua
@@ -1,6 +1,5 @@
 -- Bootstrap.lua
 -- Xal's Xpedited Routes
--- Originally based on "WhereIGathered" by fytta. Used and extended with credit to the original author.
 local addonName, addonTable = ...
 
 -- Main namespace definition
